e96ed7aa6034ce48eb6b489a79f1098e95a5731e
rachel <jrachelr1@gmail.com>
2024-03-03 07:18:41 -0800

    need to add explicit wait

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import os
-from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver import Keys, ActionChains
-
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
 
 
 # params
@@ -17,42 +18,71 @@ url = "https://www.travelyosemite.com/lodging/housekeeping-camp"
 options = Options()
 # options.add_argument("--headless")
 arrival_input_id = "container-widget-hero_ArrivalDate"
-arrival_date = "5/30/2024"
+# arrival_date = "5/30/2024"
+arrival_date = "4/22/2024"
 depart_input_id = "container-widget-hero_DepartureDate"
-depart_date = "6/2/2024"
-submit_button_name = "wxa-form-button-submit"
-xpath = "/html/body/div[9]/div/div/div/div[2]/div[2]/div/form/div[10]/input"
-
-driver = webdriver.Chrome(
-    service=ChromeService(ChromeDriverManager().install()), options=options
+# depart_date = "6/2/2024"
+depart_date = "4/23/2024"
+submit_button_xpath = (
+    "/html/body/div[9]/div/div/div/div[2]/div[2]/div/form/div[10]/input"
 )
+avail_dates_xpath = "/html/body/div[2]/div/div[3]/div/div[2]/div[1]/div/h4[2]"
+BOT_API_KEY = os.environ.get("BOT_API_KEY")
+TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
 
-driver.get(url)
 
-arrival_input_element = driver.find_element(By.ID, arrival_input_id)
-arrival_input_element.clear()
-arrival_input_element.send_keys(arrival_date)
+def check_availability():
+    dates_available = False
 
-depart_input_element = driver.find_element(By.ID, depart_input_id)
-depart_input_element.clear()
-depart_input_element.send_keys(depart_date)
+    # access search page
+    driver = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()), options=options
+    )
+    driver.get(url)
 
-# sleep(5)
-submit_button_element = driver.find_element(By.XPATH, xpath)
-print("button: ", submit_button_element.get_attribute("value"))
-# action = ActionChains(driver)
-# action.move_to_element(submit_button_element)
-# action.click(submit_button_element)
-submit_button_element.click()
+    # input arrival date
+    arrival_input_element = driver.find_element(By.ID, arrival_input_id)
+    arrival_input_element.clear()
+    arrival_input_element.send_keys(arrival_date)
 
-sleep(5)
-# submit_button_element.click()
-no_results = "box-results-empty-outer"
+    # input depart date
+    depart_input_element = driver.find_element(By.ID, depart_input_id)
+    depart_input_element.clear()
+    depart_input_element.send_keys(depart_date)
 
-try:
-    no_results_element = driver.find_element(By.ID, no_results)
-    print("no results: ", no_results)
-except Exception:
-    sleep(5)
-    heading = driver.find_element(By.TAG_NAME, "h3")
-    print("h3: ", heading.get_attribute("innerHTML"))
+    # submit form
+    submit_button_element = driver.find_element(By.XPATH, submit_button_xpath)
+    submit_button_element.click()
+
+    try:
+        driver.implicitly_wait(5)
+        avail_dates = driver.find_element(By.XPATH, avail_dates_xpath)
+        avail_dates_string = avail_dates.get_attribute("innerHTML")
+        print("avail dates: ", avail_dates_string)
+        dates_available = True
+        return dates_available, avail_dates_string
+
+    except Exception:
+        driver.implicitly_wait(5)
+        no_results = "box-results-empty-outer"
+        no_results_element = driver.find_element(By.ID, no_results)
+        no_results_string = no_results_element.get_attribute("innerHTML")
+        print("no results: ", no_results_string)
+        return dates_available
+
+
+def send_message(url):
+    available = check_availability()
+
+    if available:
+        message = f"Housekeeping Camp has availability!! \n book now: {url}"
+        telegram_message_url = f"https://api.telegram.org/bot{BOT_API_KEY}/sendMessage?chat_id={TELEGRAM_CHAT_ID}&text={message}"
+        print(requests.get(telegram_message_url).json())
+    else:
+        print("dates not available")
+
+
+send_message(url)
+
+# To do:
+# Update to explicit wait
