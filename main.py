fe322baf7db3d225b75d215d7721473165990da4
rachel <jrachelr1@gmail.com>
2024-03-12 16:52:59 -0700

    working as expected

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from dotenv import load_dotenv
+from time import sleep
 import requests
 
 load_dotenv()
@@ -18,11 +19,14 @@ url = "https://www.travelyosemite.com/lodging/housekeeping-camp"
 options = Options()
 # options.add_argument("--headless")
 arrival_input_id = "container-widget-hero_ArrivalDate"
-# arrival_date = "5/30/2024"
-arrival_date = "4/22/2024"
 depart_input_id = "container-widget-hero_DepartureDate"
+# arrival_date = "5/30/2024"
 # depart_date = "6/2/2024"
+
+# test  dates_available = True
+arrival_date = "4/22/2024"
 depart_date = "4/23/2024"
+
 submit_button_xpath = (
     "/html/body/div[9]/div/div/div/div[2]/div[2]/div/form/div[10]/input"
 )
@@ -53,28 +57,31 @@ def check_availability():
     # submit form
     submit_button_element = driver.find_element(By.XPATH, submit_button_xpath)
     submit_button_element.click()
+    sleep(5)
+
+    wait = WebDriverWait(driver, timeout=5)
 
     try:
-        driver.implicitly_wait(5)
-        avail_dates = driver.find_element(By.XPATH, avail_dates_xpath)
-        avail_dates_string = avail_dates.get_attribute("innerHTML")
+        avail_dates_element = wait.until(
+            EC.visibility_of_element_located((By.XPATH, avail_dates_xpath))
+        )
+        avail_dates_string = avail_dates_element.get_attribute("innerHTML")
         print("avail dates: ", avail_dates_string)
         dates_available = True
-        return dates_available, avail_dates_string
+        return dates_available
 
     except Exception:
-        driver.implicitly_wait(5)
-        no_results = "box-results-empty-outer"
-        no_results_element = driver.find_element(By.ID, no_results)
+        no_results_id = "box-results-empty-outer"
+        no_results_element = driver.find_element(By.ID, no_results_id)
         no_results_string = no_results_element.get_attribute("innerHTML")
         print("no results: ", no_results_string)
         return dates_available
 
 
 def send_message(url):
-    available = check_availability()
+    dates_available = check_availability()
 
-    if available:
+    if dates_available:
         message = f"Housekeeping Camp has availability!! \n book now: {url}"
         telegram_message_url = f"https://api.telegram.org/bot{BOT_API_KEY}/sendMessage?chat_id={TELEGRAM_CHAT_ID}&text={message}"
         print(requests.get(telegram_message_url).json())
@@ -83,6 +90,3 @@ def send_message(url):
 
 
 send_message(url)
-
-# To do:
-# Update to explicit wait
